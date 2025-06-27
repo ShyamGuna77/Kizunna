0b061fa1b229f3cd37113a761503d80776d284ce
Shyam <gshyamprasad2001@gmail.com>
2025-06-27 07:01:26 +0530

    wip

--- kiznew/src/app/Chats/Page.tsx
+++ kiznew/src/app/Chats/Page.tsx
@@ -1,5 +1,5 @@
 
-import React from "react";
+
 import Link from "next/link";
 import Image from "next/image";
 import { MessageCircle, ArrowRight, Inbox, Send } from "lucide-react";
--- kiznew/src/app/match/page.tsx
+++ kiznew/src/app/match/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import Link from "next/link";
 import Image from "next/image";
 import { Heart, MessageCircle, Eye, MapPin } from "lucide-react";
